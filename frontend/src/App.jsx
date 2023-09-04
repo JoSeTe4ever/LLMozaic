0ba6dc4fbe59411569ee2d9a8d738b04d0d94387
Francois Gutierrez <GutierJF@basfad.basf.net>
2023-09-04 17:52:17 +0200

    disable getEmails from begining

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,109 +5,109 @@ import Layout from './components/Layout';
 import Chat from './Chat';
 
 function App() {
-  const nylas = useNylas();
-  const [userId, setUserId] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [emails, setEmails] = useState([]);
-  const [toastNotification, setToastNotification] = useState('');
-  const SERVER_URI = import.meta.env.VITE_SERVER_URI || 'http://localhost:9000';
+	const nylas = useNylas();
+	const [userId, setUserId] = useState('');
+	const [userEmail, setUserEmail] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [emails, setEmails] = useState([]);
+	const [toastNotification, setToastNotification] = useState('');
+	const SERVER_URI = import.meta.env.VITE_SERVER_URI || 'http://localhost:9000';
 
-  useEffect(() => {
-    if (!nylas) {
-      return;
-    }
+	useEffect(() => {
+		if (!nylas) {
+			return;
+		}
 
-    // Handle the code that is passed in the query params from Nylas after a successful login
-    const params = new URLSearchParams(window.location.search);
-    if (params.has('code')) {
-      nylas
-        .exchangeCodeFromUrlForToken()
-        .then((user) => {
-          const { id } = JSON.parse(user);
-          setUserId(id);
-          sessionStorage.setItem('userId', id);
-        })
-        .catch((error) => {
-          console.error('An error occurred parsing the response:', error);
-        });
-    }
-  }, [nylas]);
+		// Handle the code that is passed in the query params from Nylas after a successful login
+		const params = new URLSearchParams(window.location.search);
+		if (params.has('code')) {
+			nylas
+				.exchangeCodeFromUrlForToken()
+				.then((user) => {
+					const { id } = JSON.parse(user);
+					setUserId(id);
+					sessionStorage.setItem('userId', id);
+				})
+				.catch((error) => {
+					console.error('An error occurred parsing the response:', error);
+				});
+		}
+	}, [nylas]);
 
-  useEffect(() => {
-    const userIdString = sessionStorage.getItem('userId');
-    const userEmail = sessionStorage.getItem('userEmail');
-    if (userIdString) {
-      setUserId(userIdString);
-    }
-    if (userEmail) {
-      setUserEmail(userEmail);
-    }
-  }, []);
+	useEffect(() => {
+		const userIdString = sessionStorage.getItem('userId');
+		const userEmail = sessionStorage.getItem('userEmail');
+		if (userIdString) {
+			setUserId(userIdString);
+		}
+		if (userEmail) {
+			setUserEmail(userEmail);
+		}
+	}, []);
 
-  useEffect(() => {
-    if (userId?.length) {
-      window.history.replaceState({}, '', `/?userId=${userId}`);
-      getEmails();
-    } else {
-      window.history.replaceState({}, '', '/');
-    }
-  }, [userId]);
+	useEffect(() => {
+		if (userId?.length) {
+			window.history.replaceState({}, '', `/?userId=${userId}`);
+			//   getEmails();
+		} else {
+			window.history.replaceState({}, '', '/');
+		}
+	}, [userId]);
 
-  const getEmails = async () => {
-    setIsLoading(true);
-    try {
-      const url = SERVER_URI + '/nylas/read-emails';
-      const res = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: userId,
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await res.json();
-      if (Array.isArray(data)) {
-        setEmails(data);
-      } else {
-        setEmails([]);
-      }
-    } catch (e) {
-      console.warn(`Error retrieving emails:`, e);
-      return false;
-    }
-    setIsLoading(false);
-  };
+	const getEmails = async () => {
+		setIsLoading(true);
+		try {
+			const url = SERVER_URI + '/nylas/read-emails';
+			const res = await fetch(url, {
+				method: 'GET',
+				headers: {
+					Authorization: userId,
+					'Content-Type': 'application/json',
+				},
+			});
+			const data = await res.json();
+			if (Array.isArray(data)) {
+				setEmails(data);
+			} else {
+				setEmails([]);
+			}
+		} catch (e) {
+			console.warn(`Error retrieving emails:`, e);
+			return false;
+		}
+		setIsLoading(false);
+	};
 
-  const disconnectUser = () => {
-    sessionStorage.removeItem('userId');
-    sessionStorage.removeItem('userEmail');
-    setUserId('');
-    setUserEmail('');
-  };
+	const disconnectUser = () => {
+		sessionStorage.removeItem('userId');
+		sessionStorage.removeItem('userEmail');
+		setUserId('');
+		setUserEmail('');
+	};
 
-  const refresh = () => {
-    getEmails();
-  };
+	const refresh = () => {
+		getEmails();
+	};
 
-  return (
-    <Layout
-      showMenu={!!userId}
-      disconnectUser={disconnectUser}
-      refresh={refresh}
-      isLoading={isLoading}
-      title=""
-      toastNotification={toastNotification}
-      setToastNotification={setToastNotification}
-    >
-      {!userId ? (
-        <NylasLogin email={userEmail} setEmail={setUserEmail} />
-      ) : (
-        <div className="app-card">
-          <Chat></Chat>
-        </div>
-      )}
-    </Layout>
-  );
+	return (
+		<Layout
+			showMenu={!!userId}
+			disconnectUser={disconnectUser}
+			refresh={refresh}
+			isLoading={isLoading}
+			title=''
+			toastNotification={toastNotification}
+			setToastNotification={setToastNotification}
+		>
+			{!userId ? (
+				<NylasLogin email={userEmail} setEmail={setUserEmail} />
+			) : (
+				<div className='app-card'>
+					<Chat></Chat>
+				</div>
+			)}
+		</Layout>
+	);
 }
 
 export default App;
