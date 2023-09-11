19f13405d17768bdbb6d7380d0cb706fa710567a
DrZuzzjen <gutijeanf@gmail.com>
2023-09-11 21:24:24 +0200

    Better prompt

--- backend/SayHiChain.py
+++ backend/SayHiChain.py
@@ -4,17 +4,38 @@ from langchain.chains import LLMChain
 
 import dotenv
 import os
-import sys
 
+# Load environment variables
 dotenv.load_dotenv()
-OPEN_API_KEY = os.getenv("OPEN_API_KEY");
+OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 
+# Constants
+actions_menu = """
+You have CRUD over:
+- Emails
+- Calendar
+- Contacts
+"""
+
+output_format = """
+- Salutation (with no name, you can use something generic like 'buddy') + email number update + funny short satirical pun
+- Your output for the possible actions the user can do based on the following menu:
+{}
+
+""".format(actions_menu)
+
+# Custom Chain
 class SayHiChain(LLMChain):
     def __init__(self):
-        llm = OpenAI(openai_api_key=OPEN_API_KEY, temperature=0.8, model_name="gpt-4")
+        llm = OpenAI(openai_api_key=OPEN_API_KEY, temperature=0.8, model_name="gpt-3.5-turbo")
         prompt = PromptTemplate(
             input_variables=["unread_emails"],
-            template="""You are an personal assistant, You must create a fun welcome message to the user saying how many email they got -
-            Something like 'Hey user, you have {unread_emails} unread emails today!' """,
+            template=f"""You are a personal assistant. You must create a fun welcome message to the user saying how many emails they got.
+            Something like 'Hey user, you have {{unread_emails}} unread emails today!'
+            When you finish with the update, you should also offer the user a list of possible actions that they can do with you.
+            
+            Also, always format your reply like this:
+            {output_format}
+            """
         )
         super().__init__(llm=llm, prompt=prompt, verbose=True)
