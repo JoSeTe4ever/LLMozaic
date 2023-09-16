4d804e18f5c5f010f8148df9949f7a3ad27d22c8
DrZuzzjen <gutijeanf@gmail.com>
2023-09-16 13:08:40 +0200

    Offer options

--- backend/SayHiChain.py
+++ backend/SayHiChain.py
@@ -12,21 +12,29 @@ OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 # Constants
 
 output_format = """
-- Salutation (with no name, you can use something generic like 'buddy') + email number update + funny short satirical pun
-- Possible actions
-
+- Salutation (with no name, you can use something generic like 'buddy') + warm and original welcome + email number update + funny short satirical pun
+\n💡May I suggest:
+  \n[Emjoji] + Action Name
+  \n[Emjoji] + Action Name
+\n[Emjoji] + Action Name
 """
 
 # Custom Chain
+
+
 class SayHiChain(LLMChain):
     def __init__(self):
-        llm = OpenAI(openai_api_key=OPEN_API_KEY, temperature=0.8, model_name="gpt-3.5-turbo")
+        llm = OpenAI(openai_api_key=OPEN_API_KEY,
+                     temperature=0.8, model_name="gpt-3.5-turbo")
         prompt = PromptTemplate(
             input_variables=["unread_emails"],
             template=f"""You are a personal assistant. You must create a fun welcome message to the user saying how many emails they got.
             Something like 'Hey user, you have {{unread_emails}} unread emails today!'
-            When you finish with the update, you should also offer the user a list of possible actions that they can do with you.      
-            Also, always format your reply like this:
+            When you finish with the update, you should also offer the user a list of possible actions that they can do with you.
+            The actions should be related to the your assistant task. You could offer to reply or create some drafts. Try to be very helpfull as a good personal assistant
+            The should be presented as a list, 1 option per line. and no more than 3  
+     
+            Plase use this output format:
             {output_format}
             """
         )
