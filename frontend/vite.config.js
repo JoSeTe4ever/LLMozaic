bb14072fe26e5c53d96fa655559ba61ce9f84c39
Jose Herminio Parreno Piqueras <josete4ever@gmail.com>
2023-09-20 19:52:38 +0200

    fix: added nginx.conf reverse proxy and proper docker compose yml file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  },
 });
