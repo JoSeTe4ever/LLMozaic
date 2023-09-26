6a11042d01fc6ad60824596497ef9440f98e8ee5
josete4ever <josete4ever@gmail.com>
2023-09-26 19:55:40 +0000

    fix: prod changes

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -184,7 +184,7 @@ export default function Chat({ greetingInfo }) {
   useEffect(() => {
     // run the chain and set the first message
     setLoading(true);
-    const SERVER_URI = "http://localhost:5000";
+    const SERVER_URI = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
     const ENDPOINT = SERVER_URI + "/greeting-chain";
     axios
       .post(ENDPOINT, greetingInfo)
